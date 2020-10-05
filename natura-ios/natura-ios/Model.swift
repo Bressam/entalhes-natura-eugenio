be2eaaf865cea6effc971593c9c0aac41454b00f
Giovanne Bressam <giovannebressam@gmail.com>
2020-10-05 19:20:14 -0300

    fotos de produtos adicionadas

--- natura-ios/natura-ios/Model.swift
+++ natura-ios/natura-ios/Model.swift
@@ -28,9 +28,15 @@ enum ProductCategory: String, Codable {
     var image: String {
         switch self {
         case .cabelo:
-            return ""
+            return "c1_shampoo_patua"
+        case .rosto:
+            return "r1_elixir_rugas"
+        case .perfumaria:
+            return "p1_biografia"
+        case .maquiagem:
+            return "m1_pincel_blush_aquarela"
         default:
-            return ""
+            return "p1_biografia"
         }
     }
 
@@ -83,18 +89,33 @@ struct DataModel {
 
     private init() {
         let cabelo = [
-            Product(name: "Natura Lumina", description: "Shampoo/Condicionar", image: "lumina"),
-            Product(name: "Natura Lumina", description: "Máscara de tratamento", image: "lumina"),
-            Product(name: "Plant", description: "Shampoo/Condicionar", image: "lumina"),
-            Product(name: "Plant", description: "Máscara de tratamento", image: "lumina"),
-            Product(name: "Ekos Patauá", description: "Shampoo/Condicionar", image: "lumina")]
-
+            Product(name: "EKOS: Shampoo Patuá", description: "Shampoo/Condicionar", image: "c1_shampoo_patua"),
+            Product(name: "Lumina: Máscara fortificante", description: "Máscara de tratamento", image: "c2_lumina_mascara"),
+            Product(name: "PLANT: Perfume para cabelo Inspira", description: "Perfume para cabelo", image: "c3_perfunte_plant_inspira")]
+        
         let maquiagem = [
-            Product(name: "Natura", description: "Batom", image: "lumina")
-        ]
+            Product(name: "Aquarela: Pincel, pó e blush", description: "Pincel, pó e blush aquarela", image: "m1_pincel_blush_aquarela"),
+            Product(name: "UNA - Base Matte", description: "Maquinagem", image: "m2_base_matte"),
+            Product(name: "UNA: Maxxi Palette de sombras - 12 tons intensa una", description: "Paleta de sombras", image: "m3_paleta_maxxi")]
+        
+        let rosto = [
+            Product(name: "Chronos: Elixir redutor de rugas", description: "Elixir redutor de rugas faciais", image: "r1_elixir_rugas"),
+            Product(name: "Chronos: Esfoliante antissinais", description: "Esfoliante antissinais", image: "r2_esfoliante_antissinais"),
+            Product(name: "Chronos: Máscara de Argila Purificante", description: "Máscara facial", image: "r3_chronos_mascara"),
+            Product(name: "Chronos: Elixir redutor de rugas", description: "Elixir redutor de rugas faciais", image: "r3_elixir_rugas"),
+            Product(name: "Faces: Sabonete gel para o rosto", description: "Sabonete gel", image: "r4_sabonete_gel")]
+        
+        let perfumaria = [
+            Product(name: "BIOGRAFIA: Perfume BIOGRAFIA feminino", description: "Perfume Feminino", image: "p1_biografia"),
+            Product(name: "LUNA: Perfume LUNA intenso", description: "Perfume Feminino", image: "p2_luna"),
+            Product(name: "Essencial Exclusivo", description: "Perfume Feminino", image: "p3_essencial_feminino"),
+            Product(name: "Essencial Exclusivo", description: "Perfume Feminino", image: "p3_essencial_feminino"),
+            Product(name: "Kaiak Aero", description: "Perfume Masculino", image: "p4_aero_masculino")]
 
         self.products[.cabelo] = cabelo
         self.products[.maquiagem] = maquiagem
+        self.products[.rosto] = rosto
+        self.products[.perfumaria] = perfumaria
     }
 }
 
