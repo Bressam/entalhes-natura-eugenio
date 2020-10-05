b58abc093b4988c0d8499d05518486efa0f10011
Adonay Puszczynski <adonay.puszczynski@livetouch.com.br>
2020-10-04 22:04:11 -0300

    pod install

---
+++ natura-ios/Pods/Target Support Files/Pods-natura-ios/Pods-natura-ios-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_natura_ios : NSObject
+@end
+@implementation PodsDummy_Pods_natura_ios
+@end
--- natura-ios/natura-ios/AppDelegate.swift
+++ natura-ios/natura-ios/AppDelegate.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+import IQKeyboardManager
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        IQKeyboardManager.shared().isEnabled = true
+        IQKeyboardManager.shared().shouldResignOnTouchOutside = true
+        IQKeyboardManager.shared().toolbarDoneBarButtonItemText = "Feito"
         return true
     }
 
